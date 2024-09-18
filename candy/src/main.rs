5e8ee30812466fd8345af5093e0ef1a0ff60fa07
Ostap Tymchenko <ostap.tymchenko@gmail.com>
2024-09-18 12:18:03 -0400

    Candy, about to remove old strat

--- candy/src/main.rs
+++ candy/src/main.rs
@@ -6,10 +6,7 @@
 // Children with a higher rating get more candies than their neighbors.
 // Return the minimum number of candies you need to have to distribute the candies to the children.
 
-fn main() {
-    let example_1 = vec![1, 0, 2];
-    dbg!(candy(example_1));
-}
+use std::usize;
 
 fn i32_to_usize(i: i32) -> usize {
     if i <= 0 {
@@ -19,38 +16,103 @@ fn i32_to_usize(i: i32) -> usize {
     }
 }
 
+fn main() {
+    let example_1 = vec![1, 5, 2,];
+    dbg!(candy(example_1));
+}
+
+// fn determine_current_child_candy(ratings: Vec<i32>, child_num: usize) -> i32 {
+//     let mut give_candy = 0;
+//
+//     let rate = ratings[child_num];
+//     dbg!(rate);
+//
+//     let left_child_rate: &i32 = &ratings
+//         .get(i32_to_usize(child_num as i32 - 1))
+//         .unwrap_or(&0);
+//     let right_child_rate: &i32 = &ratings
+//         .get(i32_to_usize(child_num as i32 + 1))
+//         .unwrap_or(&0);
+//
+//     if rate == 0 {
+//         give_candy = 1;
+//     } 
+//
+//     println!("START rate:{rate}");
+//
+//     if rate > *left_child_rate && rate > *right_child_rate && child_num != 0 && child_num + 1 != ratings.len() {
+//         if *left_child_rate == 0 {
+//             println!("END rate:2, A");
+//             return 2;
+//         } else {
+//             println!("END rate:{}, B", left_child_rate + 1);
+//             return left_child_rate + 1;
+//         }
+//     } else if rate > *left_child_rate && child_num != 0 {
+//         if *left_child_rate == 0 {
+//             println!("END rate:2, C");
+//             return 2;
+//         } else {
+//             println!("END rate:{}, D", left_child_rate +1);
+//             return left_child_rate + 1;
+//         }
+//     } else if rate > *right_child_rate && child_num + 1 != ratings.len() {
+//         dbg!(ratings.len(), child_num);
+//
+//         if *right_child_rate == 0 {
+//             println!("END rate:2, E");
+//             return 2;
+//         } else {
+//             println!("END rate:{}, F", right_child_rate +1);
+//             return right_child_rate + 1;
+//         }
+//     } else {
+//         println!("END rate:1, G");
+//         return 1;
+//     }
+// }
+
+fn layered_increase_strategy(ratings: Vec<i32>) -> i32 {
+
+    let candy_array = vec![1;ratings.len()];
+
+    dbg!(&candy_array);
+
+    for (child_num, current_rate) in ratings.iter().enumerate() {
+
+        let left_rate: &i32 = &ratings
+            .get(i32_to_usize(child_num as i32 - 1))
+            .unwrap_or(&-1);
+
+        let right_rate: &i32 = &ratings
+            .get(i32_to_usize(child_num as i32 + 1))
+            .unwrap_or(&-1);
+        }
+
+    candy_array.iter().sum()
+}
+
 pub fn candy(ratings: Vec<i32>) -> i32 {
-    let mut candy = 0;
+    layered_increase_strategy(ratings)
+}
 
-    for (child_num, rate) in ratings.iter().enumerate() {
-        let mut number_of_candy_for_this_child = 0;
+#[cfg(test)]
+mod test {
+    use super::*;
 
-        let left_child_rate: &i32 = &ratings.get(i32_to_usize(child_num as i32 - 1)).unwrap_or(&0);
-        let right_child_rate: &i32 = &ratings.get(i32_to_usize(child_num as i32 + 1)).unwrap_or(&0);
-
-        let mut top = rate;
-        let mut bottom = rate;
-
-        if left_child_rate > top {
-            top = left_child_rate;
-        } else if left_child_rate < top {
-            bottom = left_child_rate;
-        }
-
-        if right_child_rate > top {
-            top = right_child_rate;
-        } else if left_child_rate < top {
-            bottom =right_child_rate;
-        }
-
-        if rate == top {
-            // Heighest case
-        } else if  rate == bottom {
-            // Lowest case
-            number_of_candy_for_this_child = 1;
-        }
-
+    #[test]
+    fn test_candy() {
+        assert_eq!(candy(vec![1, 0, 2]), 5);
     }
 
-    candy
+    #[test]
+    fn test_candy_2() {
+        assert_eq!(candy(vec![1, 2, 2]), 4);
+        //                    1  2, 1
+    }
+
+    #[test]
+    fn test_candy_3() {
+        assert_eq!(candy(vec![29,51,87,87,72,12]), 12);
+    }
 }
---
+++ candy/target/debug/deps/candy-143f1b77b289de4b.d
@@ -0,0 +1,5 @@
+/home/cerno/Projects/leetcode/candy/target/debug/deps/candy-143f1b77b289de4b: src/main.rs
+
+/home/cerno/Projects/leetcode/candy/target/debug/deps/candy-143f1b77b289de4b.d: src/main.rs
+
+src/main.rs:
